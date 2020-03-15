91764d8fe8f31666f405b400d43711d6dfeb6826
aaronmhr <aaronmhr@gmail.com>
2020-03-15 17:56:08 +0100

    feature - Add `CurrencyRepository`

--- Converter/Repositories/CurrencyRepository.swift
+++ Converter/Repositories/CurrencyRepository.swift
@@ -1,11 +1,11 @@
 //
-//  CurrencyProvider.swift
+//  CurrencyRepository.swift
 //  Converter
 //
-//  Created by Aaron Huánuco on 14/03/2020.
+//  Created by Aaron Huánuco on 15/03/2020.
 //  Copyright © 2020 Aaron Huánuco. All rights reserved.
 //
 
-public protocol CurrencyProvider {
+public protocol CurrencyRepository {
     func getCurrencies() -> [Currency]
 }
