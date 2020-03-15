6be9436202de288c20f7d148475dc142259207fb
aaronmhr <aaronmhr@gmail.com>
2020-03-15 18:02:18 +0100

    feature - Add `TestingViewController` and stablish it as the first screen

---
+++ RCC/TestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewController.swift
+//  RCC
+//
+//  Created by Aaron Huánuco on 15/03/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import UIKit
+
+final class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
