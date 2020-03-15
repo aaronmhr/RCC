efe3f858affa3432bbe62bfceb4fa87ce75568d2
aaronmhr <aaronmhr@gmail.com>
2020-03-15 17:32:22 +0100

    data-presentation - Add `Data and `Presentation` frameworks

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Aaron Huánuco on 15/03/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
