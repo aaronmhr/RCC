25f1835d48c4bbc647177a92b3c9ecb8b82ac889
aaronmhr <aaronmhr@gmail.com>
2020-03-17 17:36:07 +0100

    pair-provider - Change interface og `PairProvider` and `PairSaver`

--- Converter/UseCases/PairProvider/PairProvider.swift
+++ Converter/UseCases/PairProvider/PairProvider.swift
@@ -1,15 +1,11 @@
 //
-//  ExchangePairProvider.swift
+//  PairProvider.swift
 //  Converter
 //
 //  Created by Aaron Huánuco on 15/03/2020.
 //  Copyright © 2020 Aaron Huánuco. All rights reserved.
 //
 
-public protocol ExchangePairProvider {
+public protocol PairProvider {
     func getConfiguredPairs() -> [Pair]
-}
-
-public protocol ExchangePairSaver {
-    func save(_ pair: Pair)
 }
