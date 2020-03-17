e2813f41f11fc2b2a4ebcd1e83b42b51c0b00414
aaronmhr <aaronmhr@gmail.com>
2020-03-17 17:50:19 +0100

    pair-provider - Rename interfaces to PairProvider and PairSaver

---
+++ Converter/UseCases/PairInteractor.swift
@@ -0,0 +1,12 @@
+//
+//  PairInteractor.swift
+//  Converter
+//
+//  Created by Aaron Huánuco on 17/03/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+
+final class PairInteractor {
+    
+}
---
+++ ConverterTests/UseCases/PairInteractor/PairInteractorTests.swift
@@ -0,0 +1,21 @@
+//
+//  PairInteractorTests.swift
+//  Converter
+//
+//  Created by Aaron Huánuco on 17/03/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Converter
+
+final class PairInteractorTests: QuickSpec {
+    override func spec() {
+        var sut: PairInteractor!
+        
+        beforeEach {
+            sut = PairInteractor()
+        }
+    }
+}
