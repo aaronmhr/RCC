f6d704c9c6efca8d235a7e193ea1c2da84c38f89
aaronmhr <aaronmhr@gmail.com>
2020-03-21 17:18:13 +0100

    rates - Add Rates cell

--- RCC/Rates/View/Cells/RatesCell.swift
+++ RCC/Rates/View/Cells/RatesCell.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2020 Aaron Huánuco. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Presentation
 
-//final class RatesCell: UITableViewCell {
-//    
-//}
+final class RatesCell: UITableViewCell, NibReusable {
+    @IBOutlet private var originTitle: UILabel!
+    @IBOutlet private var originSubtitle: UILabel!
+    @IBOutlet private var destinationTitle: UILabel!
+    @IBOutlet private var destinationSubtitle: UILabel!
+    
+    public func configureCell(_ cell: ExchangePairView) {
+        originTitle.text = cell.origin.title
+        originSubtitle.text = cell.origin.subtitle
+        destinationTitle.text = cell.destination.title
+        destinationSubtitle.text = cell.destination.subtitle
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        originTitle.text = nil
+        originSubtitle.text = nil
+        destinationTitle.text = nil
+        destinationSubtitle.text = nil
+    }
+}
